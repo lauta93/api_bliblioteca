f3b4a8316a2a2dba9a2c6e3100dbcd30d0dca958
lauta93 <lauta.boehler@gmail.com>
2024-03-07 23:05:11 -0300

    se realizo la actividad nro6

--- routes/libros.js
+++ routes/libros.js
@@ -1,46 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const Libro = require("../models/Libros");
-
+const Libro = require("../models/Libro");
+// Importamos la librería para validar scopes
+const { requiredScopes } = require("express-oauth2-jwt-bearer");
 // Ruta para obtener todos los libros
-router.get("/", async (req, res) => {
-    try {
-        const libros = await Libro.find();
-        res.json(libros);
-    } catch (error) {
-        res.status(500).json({ error: "Error al obtener los libros" });
-    }
+router.get("/", requiredScopes("read:libros"), async (req, res) => {
+try {
+const libros = await Libro.find();
+res.json(libros);
+} catch (error) {
+res.status(500).json({ error: "Error al obtener los libros" });
+}
 });
 // Ruta para crear un nuevo Libro
-router.post("/", async (req, res) => {
+router.post("/", requiredScopes("write:libros"), async (req, res) => {
     try {
-        const nuevoLibro = new Libro(req.body);
-        await nuevoLibro.save();
-        res.json(nuevoLibro);
+    const nuevoLibro = new Libro(req.body);
+    await nuevoLibro.save();
+    res.json(nuevoLibro);
     } catch (error) {
-        res.status(500).json({ error: "Error al crear el Libro" });
+    res.status(500).json({ error: "Error al crear el Libro" });
     }
-});
-// Ruta para actualizar un Libro existente
-router.put("/:id", async (req, res) => {
+    });
+    // Ruta para actualizar un Libro existente
+    router.put("/:id", requiredScopes("write:libros"), async (req, res) => {
     try {
-        const Libro = await Libro.findByIdAndUpdate(req.params.id, req.body,
-            {
-                new: true,
-            });
-        res.json(Libro);
+    const Libro = await Libro.findByIdAndUpdate(req.params.id, req.body,
+    {
+    new: true,
+    });
+    res.json(Libro);
     } catch (error) {
-        res.status(500).json({ error: "Error al actualizar el Libro" });
+    res.status(500).json({ error: "Error al actualizar el Libro" });
     }
-});
-// Ruta para eliminar un Libro
-router.delete('/:id', async (req, res) => {
+    });
+    // Ruta para eliminar un Libro
+    router.delete("/:id", requiredScopes("write:libros"), async (req, res) => {
     try {
-        await Libro.findByIdAndDelete(req.params.id);
-        res.json({ message: 'Libro eliminado correctamente' });
+    await Libro.findByIdAndDelete(req.params.id);
+    res.json({ message: "Libro eliminado correctamente" });
     } catch (error) {
-        res.status(500).json({ error: 'Error al eliminar el Libro' });
+    res.status(500).json({ error: "Error al eliminar el Libro" });
     }
-});
-
-module.exports = router;
+    });
+    module.exports = router;
+    
